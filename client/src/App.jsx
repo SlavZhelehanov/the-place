6b487391e0c1fb328fe509caa8bfddb14eb9f26d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-19 16:03:54 +0200

    Add basic 404 page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router";
 
 import './App.css';
 import Home from "./components/home/Home";
+import PageNotFound from "./components/404/PageNotFound";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     )
 }
