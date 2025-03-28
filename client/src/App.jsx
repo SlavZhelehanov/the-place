dd4a160aa822cf4bcf72fc4a56df96759027ced3
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-28 21:22:10 +0200

    Refactor App jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import { Routes, Route } from "react-router";
+import { Route, Routes } from 'react-router';
 
 import './App.css';
-import Home from "./components/home/Home";
-import PageNotFound from "./components/404/PageNotFound";
+
+import Navigation from "./components/navigation/Navigation";
+import RegisterForm from './components/auth/RegisterForm';
+import LoginForm from './components/auth/LoginForm';
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <>
+            <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 w-full bg-white">
+                <Navigation />
+            </div>
+
+
+
+            <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-slate-100">
+                <Routes>
+                    <Route index element={<h1>Home</h1>} />
+                    <Route path='/auth/register' element={<RegisterForm />} />
+                    <Route path='/auth/login' element={<LoginForm />} />
+                </Routes>
+                <p>Somet text</p>
+            </div>
+        </>
     )
 }
