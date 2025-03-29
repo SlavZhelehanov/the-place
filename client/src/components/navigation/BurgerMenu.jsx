abffd3862a682162b4cb022157b3a7752506aab7
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-29 22:27:10 +0200

    Refactor burger menu

--- client/src/components/navigation/BurgerMenu.jsx
+++ client/src/components/navigation/BurgerMenu.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import { FaHouse, FaUsers, FaUserGroup, FaBookOpenReader, FaUserPlus, FaArrowRightToBracket } from "react-icons/fa6";
+import { FaHouse, FaUsers, FaUserGroup, FaBookOpenReader, FaUserPlus, FaArrowRightToBracket, FaArrowRightFromBracket } from "react-icons/fa6";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function BurgerMenu() {
+    const { isAuthenticated } = useAuth() || {};
     const [spread, setSpread] = useState();
 
     return (
@@ -16,13 +18,17 @@ export default function BurgerMenu() {
             {spread &&
                 <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
                     <Link to="/" className="flex items-center gap-2"><FaHouse className="text-orange-400" /><span>Home</span></Link>
-                    <Link to="/" className="flex items-center gap-2"><FaUserGroup className="text-orange-400" /><span>Groups</span></Link>
-                    <Link to="/" className="flex items-center gap-2"><FaBookOpenReader className="text-orange-400" /><span>Stories</span></Link>
-                    <Link to="/" className="flex items-center gap-2"><FaUsers className="text-orange-400" /><span>Friends</span></Link>
-                    <Link to="/auth/register" className="flex items-center gap-2"><FaUserPlus className="text-orange-400" /><span>Register</span></Link>
-                    <Link to="/auth/login" className="flex items-center gap-2"><FaArrowRightToBracket className="text-orange-400" /><span>Login</span></Link>
+                    {isAuthenticated ? (<>
+                        <Link to="/" className="flex items-center gap-2"><FaUserGroup className="text-orange-400" /><span>Groups</span></Link>
+                        <Link to="/" className="flex items-center gap-2"><FaBookOpenReader className="text-orange-400" /><span>Stories</span></Link>
+                        <Link to="/" className="flex items-center gap-2"><FaUsers className="text-orange-400" /><span>Friends</span></Link>
+                        <Link to="/auth/logout" className="flex items-center gap-2"><FaArrowRightFromBracket className="text-orange-400" /><span>Logout</span></Link>
+                    </>) : (<>
+                        <Link to="/auth/register" className="flex items-center gap-2"><FaUserPlus className="text-orange-400" /><span>Register</span></Link>
+                        <Link to="/auth/login" className="flex items-center gap-2"><FaArrowRightToBracket className="text-orange-400" /><span>Login</span></Link>
+                    </>)}
                 </div>
             }
-        </div>
+        </div >
     );
 }
