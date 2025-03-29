38dc34f8e87f4aeafd461e71339e4524f27b420c
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-29 22:33:30 +0200

    Modify home page

---
+++ client/src/components/home/HomePage.jsx
@@ -0,0 +1,23 @@
+import AddPost from "./center/AddPost";
+import Center from "./center/Stories";
+import LeftSide from "./leftSide/LeftSide";
+import RightSide from "./rightSide/RightSide";
+import AllPosts from "./center/AllPosts";
+
+export default function HomePage() {
+    return (
+        <div className="flex gap-6 pt-6">
+            <div className="hidden xl:block w-[20%]"><LeftSide /></div>
+
+            <div className="w-full lg:w-[70%] xl:w-[50%]">
+                <div className="flex flex-col gap-6">
+                    <Center />
+                    <AddPost />
+                    <AllPosts />
+                </div>
+            </div>
+
+            <div className="hidden lg:block w-[30%]"><RightSide /></div>
+        </div>
+    );
+}
