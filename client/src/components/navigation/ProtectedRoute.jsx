6a3dc483dd262a483991ab38d0d00cb4d35a2c80
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-30 14:49:48 +0300

    Fix navigation when type url

--- client/src/components/navigation/ProtectedRoute.jsx
+++ client/src/components/navigation/ProtectedRoute.jsx
@@ -1,24 +1,28 @@
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
 import { useAuth } from '../../contexts/AuthContext';
 import { useEffect } from 'react';
 
 export const ProtectedFromUserRoute = ({ children }) => {
+    const location = useLocation(); 
     const navigate = useNavigate();
     const { isAuthenticated } = useAuth() || {};
 
     useEffect(() => {
-        if (isAuthenticated) navigate("/");
+        if (isAuthenticated) return navigate("/");
+        return navigate(location);
     }, [isAuthenticated]);
 
     return children;
 };
 
 export const ProtectedForUserRoute = ({ children }) => {
+    const location = useLocation();    
     const navigate = useNavigate();
     const { isAuthenticated } = useAuth() || {};
 
     useEffect(() => {
-        if (!isAuthenticated) navigate("/auth/login");
+        if (!isAuthenticated) return navigate("/auth/login");
+        return navigate(location);
     }, [isAuthenticated]);
 
     return children;
