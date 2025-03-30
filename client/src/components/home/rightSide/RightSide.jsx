194357206dbf063eef7a8bc48d3885a9cff3a8a2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-30 15:57:29 +0300

    Modify right side component if there a user

--- client/src/components/home/rightSide/RightSide.jsx
+++ client/src/components/home/rightSide/RightSide.jsx
@@ -1,10 +1,18 @@
 import Promotion from "../Promotion";
+import UserCard from "../user/UserCard";
+import UserInfo from "../user/UserInfo";
 import Birthdays from "./Birthdays";
 import FriendRequests from "./FriendRequests";
 
-export default function RightSide({ userId = '' }) {
+export default function RightSide({ userId }) {
     return (
         <div className="flex flex-col gap-6">
+            {userId ?
+                (<>
+                    <UserInfo userId={userId} />
+                    <UserCard userId={userId} />
+                </>)
+                : null}
             <FriendRequests />
             <Birthdays />
             <Promotion size={"md"} />
