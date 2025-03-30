cf6a585570722570a417cfa4b3b3ab889dbb2c15
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-03-30 13:17:20 +0300

    Add right side component

--- client/src/components/home/rightSide/RightSide.jsx
+++ client/src/components/home/rightSide/RightSide.jsx
@@ -1,3 +1,13 @@
-export default function RightSide() {
-    return (<div>Right Side</div>);
+import Promotion from "../Promotion";
+import Birthdays from "./Birthdays";
+import FriendRequests from "./FriendRequests";
+
+export default function RightSide({ userId = '' }) {
+    return (
+        <div className="flex flex-col gap-6">
+            <FriendRequests />
+            <Birthdays />
+            <Promotion size={"md"} />
+        </div>
+    );
 }
