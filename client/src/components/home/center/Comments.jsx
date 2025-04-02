16617abf8b2db53e553aa6db67b3fc51e7ee6928
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-02 20:12:04 +0300

    Refactor comments component

--- client/src/components/home/center/Comments.jsx
+++ client/src/components/home/center/Comments.jsx
@@ -1,38 +1,27 @@
-export default function Comments() {
+import { useAuth } from "../../../contexts/AuthContext";
+import SingleComment from "./SingleComment";
+
+export default function Comments({
+    comments,
+    owner,
+    getComment
+}) {
+    const { isAuthenticated } = useAuth();
+
     return (
         <div className="">
 
-            <div className="flex items-center gap-4">
-                <img src="https://images.pexels.com/photos/6897336/pexels-photo-6897336.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={32} height={32} className="w-8 h-8 rounded-full" />
+            {isAuthenticated && <form action={getComment} className="flex items-center gap-4">
+                <img src={owner?.avatar} alt="" width={32} height={32} className="w-8 h-8 rounded-full" />
 
                 <div className="flex-1 flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
-                    <input type="text" placeholder="Write a comment..." className="bg-transparent outline-none flex-1" />
+                    <input type="text" name="content" placeholder="Write a comment..." className="bg-transparent outline-none flex-1" />
                     <img src="emoji.jpg" alt="" width={16} height={16} className="cursor-pointer" />
+                    <button className="cursor-pointer rounded-xl bg-orange-300 text-white hover:bg-orange-400 w-20 h-12">Sent</button>
                 </div>
-            </div>
+            </form>}
 
-            <div className="">
-                <div className="flex gap-4 justify-between mt-6">
-
-                    <img src="https://images.pexels.com/photos/6897336/pexels-photo-6897336.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={40} height={40} className="w-10 h-10 rounded-full" />
-
-                    <div className="flex flex-col gap-2 flex-1">
-                        <span className="font-medium">Bud Spencer</span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat accusantium optio reprehenderit modi culpa voluptates sequi ullam sunt autem. Odio, temporibus reprehenderit voluptates cumque et ea quasi. Eos, fugiat beatae!</p>
-                        <div className="flex items-center gap-8 text-xs text-gray-500 mt-2">
-                            <div className="flex items-center gap-4">
-                                <img src="like.jpg" alt="" width={16} height={16} className="cursor-pointer" />
-                                <span className="text-gray-300">|</span>
-                                <span className="text-gray-500">123 Likes</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <img src="/more.png" alt="" width={16} height={16} className="cursor-pointer w-4 h-4" />
-
-                </div>
-
-            </div>
+            {comments?.map(commentData => <SingleComment key={commentData._id} commentData={commentData} />)}
 
         </div>
     );
