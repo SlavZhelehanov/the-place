83e6c8ff1e7c14f082165f76a2dc67e8af7c1a13
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-02 20:13:10 +0300

    List all created users on top of the home page

--- client/src/components/home/center/Stories.jsx
+++ client/src/components/home/center/Stories.jsx
@@ -1,63 +1,36 @@
+import { use, useEffect, useState } from "react";
+import auth from "../../../utils/fetchUser";
+import Spinner from "../../Spinner";
+import { Link } from "react-router";
+
 export default function Center() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        auth().then(data => {
+            setUsers(Object.values(data));
+            setLoading(false);
+        }).catch(err => setError(err.message)).finally(() => setLoading(false));
+    }, []);
+
     return (
         <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs hide-scrollbar">
             <div className="flex gap-8 w-max">
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <img src="https://images.pexels.com/photos/31360839/pexels-photo-31360839/free-photo-of-monochrome-basketball-shot-in-motion.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Random</span>
-                </div>
+                {loading
+                    ? <div className="flex flex-col items-center gap-2 cursor-pointer">
+                        <Spinner />
+                    </div>
+                    : users.map(user => {
+                        return (
+                            <Link to={`/auth/${user._id}/profile`} key={user._id} className="flex flex-col items-center gap-2 cursor-pointer">
+                                <img src={user.avatar} alt={user.username} width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+                                <span className="font-medium">{user.fullName}</span>
+                            </Link>
+                        )
+                    })
+                }
             </div>
         </div>
     );
