05bcdf73f28f64cf0318710646b71345923ef4d0
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-02 20:10:24 +0300

    Dynamically render all created posts

--- client/src/components/home/center/AllPosts.jsx
+++ client/src/components/home/center/AllPosts.jsx
@@ -1,16 +1,15 @@
+import { useEffect, useState } from "react";
 import Post from "./Post";
 
-export default function AllPosts() {
+export default function AllPosts({ posts }) {
+    const [printPosts, setPrintPosts] = useState([]);
+
+    useEffect(() => {
+        setPrintPosts(posts);
+    }, [posts]);
     return (
         <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            {printPosts && printPosts.map(post => <Post key={post._id} post={post} />)}
         </div>
     );
 }
